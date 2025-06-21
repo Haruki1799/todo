e597e19dceff6ac6d6ae60da943eddace08c111a
haru <kobaharumail@icloud.com>
2025-06-22 05:18:37 +0900

    3-5演習

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use BadFunctionCallException;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
+
 
 class CategoryController extends Controller
 {
@@ -12,5 +15,14 @@ class CategoryController extends Controller
         $categories = Category::all();
 
         return view('category', compact('categories'));
+    }
+    public function store(CategoryRequest $request)
+    {
+    $category = $request->only(['name']);
+    Category::create($category);
+
+    session()->flash('message', 'カテゴリを作成しました');
+
+        return redirect('/categories');
     }
 }
